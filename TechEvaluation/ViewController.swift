45063a7c66af562624c9ef391612f200733200ea
kavimani <tokavi84@gmail.com>
2019-07-31 18:32:40 +0530

    Initial Commit

---
+++ TechEvaluation/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TechEvaluation
+//
+//  Created by kavimani on 31/07/19.
+//  Copyright © 2019 kavimani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
